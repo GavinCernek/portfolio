82acf37fd82bc488d59a495a0def0a4b9937b5d8
Gavin Cernek <gcernek@gmail.com>
2020-06-02 12:05:34 -0500

    Updates

---
+++ precache-manifest.50cafd5b38a3b43640623397206d5a6f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b29e589bd009373388c5d403812528e",
+    "url": "/gavincernek.github.io/index.html"
+  },
+  {
+    "revision": "6fd9e7bce9d281972216",
+    "url": "/gavincernek.github.io/static/css/main.597daa0e.chunk.css"
+  },
+  {
+    "revision": "65456629fea8a01081b1",
+    "url": "/gavincernek.github.io/static/js/2.1ed5a4f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gavincernek.github.io/static/js/2.1ed5a4f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd9e7bce9d281972216",
+    "url": "/gavincernek.github.io/static/js/main.3a69e408.chunk.js"
+  },
+  {
+    "revision": "4f951b7d8695a7ac9594",
+    "url": "/gavincernek.github.io/static/js/runtime-main.a81d6d48.js"
+  },
+  {
+    "revision": "209db0b161cec0b3fbed2af1c8e3dc05",
+    "url": "/gavincernek.github.io/static/media/Web_Resume.209db0b1.pdf"
+  },
+  {
+    "revision": "9a9a9d38809ea9b15e72bd3aa44f250b",
+    "url": "/gavincernek.github.io/static/media/chicago.9a9a9d38.png"
+  },
+  {
+    "revision": "248144d1538eb0743c3bb8284226a808",
+    "url": "/gavincernek.github.io/static/media/placeholder.248144d1.png"
+  },
+  {
+    "revision": "c08a6a887488c9cc7178282e2dcda24f",
+    "url": "/gavincernek.github.io/static/media/testCoco.c08a6a88.png"
+  },
+  {
+    "revision": "a02d1483b6d88e2031bc41f4939b5573",
+    "url": "/gavincernek.github.io/static/media/testTrophy.a02d1483.JPG"
+  },
+  {
+    "revision": "9c9ef73bc2ffb71732f12c9f638a56c7",
+    "url": "/gavincernek.github.io/static/media/test_image.9c9ef73b.png"
+  }
+]);
